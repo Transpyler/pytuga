1ebeb9928970d3b89acd88bcd7dcb47a8150922f
fabiomacedomendes <fabiomacedomendes@gmail.com>
2017-04-03 15:15:36 -0300

    tweak setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 import os
 import sys
 from glob import glob
+from setuptools import setup, find_packages
 
-from setuptools import find_packages, setup
-from setuptools.command.develop import develop as _develop
-from setuptools.command.install import install as _install
 
-NAME = 'pytuga'
-AUTHOR = 'Fábio Macêdo Mendes'
-DIRNAME = os.path.dirname(__file__)
-
-# cx_Freeze: dependencies are automatically detected, but it might need
-# fine tuning.
+# cx_Freeze: we added a undocumented option to enable building frozen versions
+# of our packages. This should be refactored for a more safe approach in the
+# future.
 setup_kwargs = {}
 if '--cx-freeze' in sys.argv:
     from cx_Freeze import setup, Executable
@@ -20,7 +15,8 @@ if '--cx-freeze' in sys.argv:
         'include_files': [],
         'packages': ['os', 'pytuga', 'pygments'],
         'excludes': [
-            'tkinter', 'redis', 'lxml', 'qturtle.qsci.widgets',
+            'tkinter', 'redis', 'lxml',
+            'qturtle.qsci.widgets',
             'nltk', 'textblob',
             'matplotlib', 'scipy', 'numpy', 'sklearn',
             'notebook',
@@ -44,81 +40,84 @@ if '--cx-freeze' in sys.argv:
     sys.argv.remove('--cx-freeze')
 
 
-# Warn user about missing PyQt libraries. These cannot go into REQUIRES list
-# since PyQt is not instalable via pip
-try:
-    import PyQt5.QtSvg
-    import PyQt5.Qsci
-except ImportError:
-    import warnings
-
-    warnings.warn(
-        'Please install PyQt5, PyQt5.QtSvg and PyQt5.Qsci!\n'
-        'Check your distribution packages or go to the website bellow:\n'
-        '    https://riverbankcomputing.com/software/pyqt/download5\n')
-
-# Rewrite __version__.py in tugalib
-VERSION = open('VERSION', encoding='utf8').read().strip()
-version_file = os.path.join(DIRNAME, 'src', 'pytuga', '__meta__.py')
-try:
-    with open(version_file, 'w', encoding='utf8') as F:
-        F.write('__version__ = %r\n'
-                '__author__ = %r' % (VERSION, AUTHOR))
-except UnicodeDecodeError:
-    # Read the docs builder has a problem with encoding. Maybe old Python3
-    # versions?
-    pass
-
-# Collect data files
-if sys.platform.startswith('win'):
-    # TODO: figure out where these files should be in Windows
-    DATA_FILES = []
-else:
-    DATA_FILES = [
-        ('share/icons/hicolor/scalable/apps', ['data/icons/pytuga.svg']),
-        ('share/icons/hicolor/scalable/mimetypes',
-         ['data/icons/text-x-pytuga.svg']),
-        ('share/mime', ['data/pytg.xml']),
-        ('share/applications', ['data/pytuga.desktop']),
-        ('share/doc/pytuga', ['README.rst']),
-        ('share/doc/examples', glob('data/examples/*.pytg')),
-        ('share/gtksourceview-3.0/language-specs', ['data/pytuga.lang']),
-    ]
-
-# Add documentation files
-for path, _, files in os.walk('doc/build/html'):
-    docfiles = []
-    DATA_FILES.append(('doc/pytuga' + path[14:], docfiles))
-    for file in files:
-        docfiles.append('%s/%s' % (path, file))
-
-# Fix path separators in windows (necessary?)
-if os.path.sep != '/':
-    for i, (path, files) in enumerate(DATA_FILES):
-        path = os.path.sep.join(path.split('/'))
-        files = [os.path.sep.join(f.split('/')) for f in files]
-        DATA_FILES[i] = (path, files)
+# Save version and author to __meta__.py
+version = open('VERSION').read().strip()
+dirname = os.path.dirname(__file__)
+path = os.path.join(dirname, 'src', 'pytuga', '__meta__.py')
+meta = '''# Automatically created. Please do not edit.
+__version__ = '%s'
+__author__ = 'F\\xe1bio Mac\\xeado Mendes'
+''' % version
+with open(path, 'w') as F:
+    F.write(meta)
 
 
-# Wraps command classes to register ipytuga kernel
-def wrapped_cmd(cmd):
-    class Command(cmd):
-        def run(self):
-            cmd.run(self)
-            # from pytuga.ipytuga.setup import setup_assets
-            # setup_assets(True)
-
-    return Command
+# FIXME: REFACTOR THOSE CHECKS
+# # Warn user about missing PyQt libraries. These cannot go into REQUIRES list
+# # since PyQt is not instalable via pip
+# try:
+#     import PyQt5.QtSvg
+#     import PyQt5.Qsci
+# except ImportError:
+#     import warnings
+#
+#     warnings.warn(
+#         'Please install PyQt5, PyQt5.QtSvg and PyQt5.Qsci!\n'
+#         'Check your distribution packages or go to the website bellow:\n'
+#         '    https://riverbankcomputing.com/software/pyqt/download5\n')
+#
+#
+# # Collect data files
+# if sys.platform.startswith('win'):
+#     # TODO: figure out where these files should be in Windows
+#     # We need someone with a windows machine! :P
+#     DATA_FILES = []
+# else:
+#     DATA_FILES = [
+#         ('share/icons/hicolor/scalable/apps', ['data/icons/pytuga.svg']),
+#         ('share/icons/hicolor/scalable/mimetypes',
+#          ['data/icons/text-x-pytuga.svg']),
+#         ('share/mime', ['data/pytg.xml']),
+#         ('share/applications', ['data/pytuga.desktop']),
+#         ('share/doc/pytuga', ['README.rst']),
+#         ('share/doc/examples', glob('data/examples/*.pytg')),
+#         ('share/gtksourceview-3.0/language-specs', ['data/pytuga.lang']),
+#     ]
+#
+# # Add documentation files
+# for path, _, files in os.walk('doc/build/html'):
+#     docfiles = []
+#     DATA_FILES.append(('doc/pytuga' + path[14:], docfiles))
+#     for file in files:
+#         docfiles.append('%s/%s' % (path, file))
+#
+# # Fix path separators in windows (necessary?)
+# if os.path.sep != '/':
+#     for i, (path, files) in enumerate(DATA_FILES):
+#         path = os.path.sep.join(path.split('/'))
+#         files = [os.path.sep.join(f.split('/')) for f in files]
+#         DATA_FILES[i] = (path, files)
+#
+#
+# # Wraps command classes to register ipytuga kernel
+# def wrapped_cmd(cmd):
+#     class Command(cmd):
+#         def run(self):
+#             cmd.run(self)
+#             # from pytuga.ipytuga.setup import setup_assets
+#             # setup_assets(True)
+#
+#     return Command
 
 
 # Run setup() function
 setup(
-    name=NAME,
-    version=VERSION,
+    name='pytuga',
+    version=version,
     description='Interpretador de Pytuguês: um Python com sotaque lusitano.',
-    author=AUTHOR,
+    author='Fábio Macêdo Mendes',
     author_email='fabiomacedomendes@gmail.com',
-    url='https://github.com/fabiommendes/pytuga',
+    url='https://github.com/transpyler/pytuga',
     long_description=('''
     Pytuguês é uma linguagem de programação que modifica a sintaxe do
     Python para aceitar comandos em português. A linguagem foi desenvolvida
@@ -146,10 +145,10 @@ setup(
     ],
 
     # Wrapped commands (for ipytuga)
-    cmdclass={
-        'install': wrapped_cmd(_install),
-        'develop': wrapped_cmd(_develop),
-    },
+    # cmdclass={
+    #    'install': wrapped_cmd(_install),
+    #    'develop': wrapped_cmd(_develop),
+    # },
 
     # Scripts
     entry_points={
@@ -160,21 +159,21 @@ setup(
     },
 
     # Data files
-    package_data={
-        'tugalinhas': [
-            '*.*',
-            'doc/html/*.*',
-            'doc/html/_modules/*.*',
-            'doc/html/_modules/tugalib/*.*',
-            'doc/html/_sources/*.*',
-            'doc/html/_static/*.*',
-            'examples/*.pytg'
-        ],
-        'pytuga': [
-            'ipytuga/assets/*.*',
-        ],
-    },
-    data_files=DATA_FILES,
+    # package_data={
+    #     'pytuga': [
+    #         '*.*',
+    #         'doc/html/*.*',
+    #         'doc/html/_modules/*.*',
+    #         'doc/html/_modules/tugalib/*.*',
+    #         'doc/html/_sources/*.*',
+    #         'doc/html/_static/*.*',
+    #         'examples/*.pytg'
+    #     ],
+    #     'pytuga': [
+    #         'ipytuga/assets/*.*',
+    #     ],
+    # },
+    # data_files=DATA_FILES,
     zip_safe=False,
     **setup_kwargs
 )
--- src/pytuga/__meta__.py
+++ src/pytuga/__meta__.py
@@ -1,2 +1,3 @@
+# Automatically created. Please do not edit.
 __version__ = '0.10.1'
-__author__ = 'Fábio Macêdo Mendes'
+__author__ = 'F\xe1bio Mac\xeado Mendes'
